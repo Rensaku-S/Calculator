c9ea0d1e1bc8c77a4b734dcca37302bb72a31d79
Rensaku-S <187811516+Rensaku-S@users.noreply.github.com>
2025-02-07 21:31:44 +0100

    Finish the calculator

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
-const firstNum = 3;
-const operator = '+';
-const lastNum = 5;
+function exponentiate(a, b) {
+    return a**b;
+}
 
 function operate(a, Op, b) {
     let result = 0;
@@ -27,49 +27,86 @@ function operate(a, Op, b) {
         result = subtract(a, b);
     } else if (Op === '*') {
         result = multiply(a, b);
+    } else if (Op === '^') {
+        result = exponentiate(a, b)
     } else {
         result = divide(a, b);
     }
     return result;
 }
 
-
 const display = document.querySelector('.display');
-const numDigit = document.querySelectorAll('.num')
-const operatorDigit = document.querySelectorAll('.operator')
-const plusMinusdigit = document.querySelector('#plus-minus')
-const clearDigit = document.querySelector('#clear')
-const dotDigit = document.querySelector('#dot')
-const egalDigit = document.querySelector('#egal')
-
-function putDigitDisplay() {
-    numDigit.forEach(num => {
-        num.addEventListener('click', () => {
-            display.textContent += num.textContent;
-        })
-    });
-}
+const numDigits = document.querySelectorAll('.num');
+const operatorDigits = document.querySelectorAll('.operator');
+const plusMinusdigit = document.querySelector('#plus-minus');
+const clearDigit = document.querySelector('#clear');
+const egalDigit = document.querySelector('#egal');
 
 function clearDisplay() {
     clearDigit.addEventListener('click', () => {
         display.textContent = '';
-    })
+        firstNum = '';
+        secondNum = '';
+        Op = '';
+        operatorJustPressed = false;
+    });
 }
 
 function signDigit() {
     plusMinusdigit.addEventListener('click', () => {
-        if (display.textContent>0) {
-            display.textContent = '-' + display.textContent
-        } else {
-            display.textContent = display.textContent.slice(1)
+        if (display.textContent < 0) {
+            display.textContent = display.textContent.slice(1);
+        } else if (display.textContent > 0) {
+            display.textContent = '-' + display.textContent;
         }
     })
 }
 
-let displayContent = display.textContent
+let firstNum = '';
+let secondNum = '';
+let Op = '';
+let operatorJustPressed = false;
 
+function takeNumDisplay() {
+    operatorDigits.forEach(operatorDigit => {
+        operatorDigit.addEventListener('click', () => {
+            if (Op !== '') {
+                secondNum = parseFloat(display.textContent);
+                let result = operate(firstNum, Op, secondNum);
+                display.textContent = result;
+                firstNum = result;
+            } else {
+                firstNum = parseFloat(display.textContent);
+            }
+            Op = operatorDigit.textContent;
+            operatorJustPressed = true;
+        });
+    });
+
+    numDigits.forEach(numDigit => {
+        numDigit.addEventListener('click', () => {
+            if (operatorJustPressed) {
+                display.textContent = numDigit.textContent;
+                operatorJustPressed = false;
+            } else {
+                display.textContent += numDigit.textContent;
+            }
+        });
+    });
+
+    egalDigit.addEventListener('click', () => {
+        secondNum = parseFloat(display.textContent);
+        let result = operate(firstNum, Op, secondNum);
+        display.textContent = result;
+
+        firstNum = parseFloat(result.textContent);
+        secondNum = '';
+        Op = '';
+        operatorJustPressed = false;
+    })
+}
 
 // Appel des fonctions
-putDigitDisplay()
 clearDisplay()
 signDigit()
+takeNumDisplay()
